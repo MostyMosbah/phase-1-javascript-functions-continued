a118bcd4f9f91e768438a73f3df5307f12d4be48
Mostafa Mosbah <mmosbah195@gmail.com>
2021-06-29 22:41:30 -0700

    commit tree 36acc8b6996165fb7e03e773fda59dafb5156a9c

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+function saturdayFun(activity= "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+function mondayWork(task= "go to the office") {
+    return `This Monday, I will ${task}.`;
+}
+mondayWork();
+
+
+function wrapAdjective(visual="*") {
+    return function(adjective="special") {
+        return `You are ${visual}${adjective}${visual}!`
+    }
+}
+
+wrapAdjective("*")("special");
